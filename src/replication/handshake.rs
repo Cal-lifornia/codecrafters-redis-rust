ce8d3cee296b8585d246112991d4414a6f86ebf3
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-08-13 13:19:47 +1000

    Reads before running follow up commands

--- src/replication/handshake.rs
+++ src/replication/handshake.rs
@@ -8,7 +8,7 @@ use tokio::{
 use crate::{
     replication::ReplicaError,
     resp::{self, Resp},
-    types::{RedisInfo, ReplicationInfo},
+    types::RedisInfo,
 };
 
 pub async fn handshake(
@@ -23,6 +23,8 @@ pub async fn handshake(
         Err(err) => return Err(Box::new(err)),
     }
 
+    expect_read(socket, "PONG").await?;
+
     match socket
         .write_all(&Resp::str_array(&["REPLCONF", "listening-port", &info.port]).to_bytes())
         .await
@@ -30,6 +32,8 @@ pub async fn handshake(
         Ok(_) => {}
         Err(err) => return Err(Box::new(err)),
     }
+
+    expect_read(socket, "OK").await?;
     match socket
         .write_all(&Resp::str_array(&["REPLCONF", "capa", "psync2"]).to_bytes())
         .await
@@ -37,35 +41,42 @@ pub async fn handshake(
         Ok(_) => {}
         Err(err) => return Err(Box::new(err)),
     }
+
+    expect_read(socket, "OK").await?;
     Ok(())
+}
 
-    // let mut buf = [0; 1024];
-    // let n = match socket.read(&mut buf).await {
-    //     Ok(0) => {
-    //         return Err(Box::new(Error::new(
-    //             std::io::ErrorKind::TimedOut,
-    //             "no return after PING",
-    //         )))
-    //     }
-    //     Ok(n) => n,
-    //     Err(err) => return Err(Box::new(err)),
-    // };
+async fn expect_read(
+    socket: &mut TcpStream,
+    expecting: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut buf = [0; 1024];
+    let n = match socket.read(&mut buf).await {
+        Ok(0) => {
+            return Err(Box::new(Error::new(
+                std::io::ErrorKind::TimedOut,
+                "no return after sending message",
+            )))
+        }
+        Ok(n) => n,
+        Err(err) => return Err(Box::new(err)),
+    };
 
-    // let response = match resp::parse(&buf[0..n]) {
-    //     Ok((response, _)) => response,
-    //     Err(err) => return Err(Box::new(err)),
-    // };
+    let response = match resp::parse(&buf[0..n]) {
+        Ok((response, _)) => response,
+        Err(err) => return Err(Box::new(err)),
+    };
 
-    // if let Resp::SimpleString(res) = response {
-    //     if res != "PONG" {
-    //         Err(Box::new(ReplicaError::UnexpectedResponse(
-    //             "PONG".to_string(),
-    //             res,
-    //         )))
-    //     } else {
-    //         Ok(())
-    //     }
-    // } else {
-    //     Err(Box::new(ReplicaError::IncorrectFormat))
-    // }
+    if let Resp::SimpleString(res) = response {
+        if res != expecting {
+            Err(Box::new(ReplicaError::UnexpectedResponse(
+                expecting.to_string(),
+                res,
+            )))
+        } else {
+            Ok(())
+        }
+    } else {
+        Err(Box::new(ReplicaError::IncorrectFormat))
+    }
 }
