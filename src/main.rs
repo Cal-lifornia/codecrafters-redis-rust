6c2928eb9181c6a418b9d8b29fba7b45a6507060
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-08-18 12:55:23 +1000

    Was using iter.skip() when I should have been using iter.step()

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use codecrafters_redis::redis::{self};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let os_args: Vec<String> = args().collect();
+    println!("os_args: {os_args:#?}");
 
     let mut port = "6379".to_string();
     let mut db_filename = String::new();
@@ -13,26 +14,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     os_args[1..]
         .iter()
-        .skip(2)
-        .zip(os_args[2..].iter().skip(2))
+        .step_by(2)
+        .zip(os_args[2..].iter().step_by(2))
         .for_each(|(key, val)| match key.as_str() {
+            "--dir" => dir = val.to_string(),
             "--port" => port = val.to_string(),
             "--replica_of" => replica_of = Some(val.to_string()),
-            "--dir" => dir = val.to_string(),
             "--dbfilename" => db_filename = val.to_string(),
             _ => {}
         });
+    let config = codecrafters_redis::types::Config {
+        dir,
+        db_filename,
+        port,
+    };
 
-    // Uncomment this block to pass the first stage
-    //
-    redis::init(
-        "127.0.0.1",
-        codecrafters_redis::types::Config {
-            dir,
-            db_filename,
-            port,
-        },
-        replica_of,
-    )
-    .await
+    println!("config: {config:#?}");
+    redis::init("127.0.0.1", config, replica_of).await
 }
