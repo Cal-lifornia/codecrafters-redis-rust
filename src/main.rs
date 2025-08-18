c0261bab03bd8390693876894c65bc0227db6630
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-08-18 13:16:02 +1000

    Wasn't chekcing for os args lengths and also got the replicaof argumetn incorrect

--- src/main.rs
+++ src/main.rs
@@ -5,30 +5,31 @@ use codecrafters_redis::redis::{self};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let os_args: Vec<String> = args().collect();
-    println!("os_args: {os_args:#?}");
 
     let mut port = "6379".to_string();
     let mut db_filename = String::new();
     let mut dir = String::new();
     let mut replica_of: Option<String> = None;
 
-    os_args[1..]
-        .iter()
-        .step_by(2)
-        .zip(os_args[2..].iter().step_by(2))
-        .for_each(|(key, val)| match key.as_str() {
-            "--dir" => dir = val.to_string(),
-            "--port" => port = val.to_string(),
-            "--replica_of" => replica_of = Some(val.to_string()),
-            "--dbfilename" => db_filename = val.to_string(),
-            _ => {}
-        });
+    if os_args.len() > 2 {
+        os_args[1..]
+            .iter()
+            .step_by(2)
+            .zip(os_args[2..].iter().step_by(2))
+            .for_each(|(key, val)| match key.as_str() {
+                "--dir" => dir = val.to_string(),
+                "--port" => port = val.to_string(),
+                "--replicaof" => replica_of = Some(val.to_string()),
+                "--dbfilename" => db_filename = val.to_string(),
+                _ => {}
+            });
+    }
+
     let config = codecrafters_redis::types::Config {
         dir,
         db_filename,
         port,
     };
 
-    println!("config: {config:#?}");
     redis::init("127.0.0.1", config, replica_of).await
 }
