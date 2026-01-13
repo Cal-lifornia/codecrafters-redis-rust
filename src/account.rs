4e9105487ce772c4486277169440ab2cba28ffbc
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2026-01-13 17:53:19 +1100

    Added nopass flag

--- src/account.rs
+++ src/account.rs
@@ -1,14 +1,40 @@
+use std::fmt::Display;
+
+use hashbrown::HashSet;
+
 #[derive(Debug, Clone)]
 pub struct Account {
     pub username: String,
-    pub flags: Vec<String>,
+    pub flags: HashSet<AccountFlag>,
 }
 
 impl Default for Account {
     fn default() -> Self {
         Self {
             username: "default".into(),
-            flags: vec![],
+            flags: HashSet::from([AccountFlag::NoPass]),
         }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+pub enum AccountFlag {
+    NoPass,
+}
+
+impl From<AccountFlag> for &[u8] {
+    fn from(value: AccountFlag) -> Self {
+        match value {
+            AccountFlag::NoPass => b"nopass",
+        }
+    }
+}
+
+impl Display for AccountFlag {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let out = match self {
+            AccountFlag::NoPass => "nopass",
+        };
+        write!(f, "{out}")
     }
 }
